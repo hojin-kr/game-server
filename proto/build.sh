a09adcb0ae060871907daf29be941d40e313fbc9
hojin <jhj377@gmail.com>
2022-02-08 23:21:50 +0900

    build csharp

--- proto/build.sh
+++ proto/build.sh
@@ -2,3 +2,7 @@
 protoc --go_out=./cmd --go_opt=paths=source_relative \
     --go-grpc_out=./cmd --go-grpc_opt=paths=source_relative \
     proto/haru.proto
+protoc --csharp_out=./proto/csharp \ 
+    --plugin=protoc-gen-csharp_grpc=/Users/hojin/Work/hojin/haru/proto/plugins/grpc_csharp_plugin \
+    --csharp_grpc_out=./proto/csharp \
+    proto/haru.proto
