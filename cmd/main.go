22c0e9d175960a137f6d043b090462ef35f3a09e
hojin <jhj377@gmail.com>
2022-07-04 02:16:30 +0900

    파싱 변경, ID로 조회 추가, 방문 기록 추가

--- cmd/main.go
+++ cmd/main.go
@@ -33,33 +33,33 @@ type server struct {
 
 // Account account infomation
 type Account struct {
-	RegisterTimestamp int64 `example:"1639056738"`
-	DeviceID          string
-	GoogleID          string
-	AppleID           string
-	LineID            string
-	KakaoID           string
-	ID                int64 `datastore:"-" example:"5373899369873408"`
+	RegisterTimestamp int64  `json:"register_timestamp,omitempty"`
+	DeviceID          string `json:"device_id,omitempty"`
+	GoogleID          string `json:"google_id,omitempty"`
+	AppleID           string `json:"apple_id,omitempty"`
+	LineID            string `json:"line_id,omitempty"`
+	KakaoID           string `json:"kakao_id,omitempty"`
+	ID                int64  `json:"id,omitempty"`
 }
 
 // Profile profile inforamtion
 type Profile struct {
-	Nickname string   `example:"myNickname"`
-	ID       int64    `datastore:"-" example:"5373899369873408"`
-	Likes    []string `example:"ID,ID,ID"`
+	Nickname string   `json:"nickname,omitempty"`
+	ID       int64    `json:"id,omitempty"`
+	Likes    []string `json:"likes,omitempty"`
 }
 
 // Wallet wallet
 type Wallet struct {
-	Coin string
-	ID   int64 `datastore:"-" example:"5373899369873408"`
+	Coin string `json:"coin,omitempty"`
+	ID   int64  `json:"id,omitempty"`
 }
 
 // Rating rating of place
 type Rating struct {
-	Point int64 `example:"100"`
-	Count int64 `example:"100"`
-	ID    int64 `datastore:"-" example:"PlaceID"`
+	Point int64 `json:"point,omitempty"`
+	Count int64 `json:"count,omitempty"`
+	ID    int64 `json:"id,omitempty"`
 }
 
 // CreateAccount implements CreateAccount
@@ -78,8 +78,8 @@ func (s *server) CreateAccount(ctx context.Context, in *pb.AccountRequest) (*pb.
 		log.Printf("Should Not Account Put:" + err.Error())
 		tracer.Trace(time.Now().UTC(), in, "Should Not Account Put", err.Error())
 	}
-	in.ID = key.ID
-	ret := &pb.AccountReply{ID: in.GetID(), RegisterTimestamp: in.GetRegisterTimestamp()}
+	in.Id = key.ID
+	ret := &pb.AccountReply{Id: in.GetId(), RegisterTimestamp: in.GetRegisterTimestamp()}
 	tracer.Trace(time.Now().UTC(), ret)
 	return ret, nil
 }
@@ -92,14 +92,14 @@ func (s *server) GetProfile(ctx context.Context, in *pb.ProfileRequest) (*pb.Pro
 		log.Printf("Should Not Datastore New Client" + err.Error())
 		tracer.Trace(time.Now().UTC(), in, "Should Not Datastore New Client", err.Error())
 	}
-	key := datastore.IDKey("Profile", in.GetID(), nil)
+	key := datastore.IDKey("Profile", in.GetId(), nil)
 	var profile Profile
 	if err := datastoreClient.Get(ctx, key, &profile); err != nil {
 		log.Printf("Should Not Profile get: " + err.Error())
 		tracer.Trace(time.Now().UTC(), in, "Should Not Profile get", err.Error())
 	}
 	in.Nickname = profile.Nickname
-	ret := &pb.ProfileReply{ID: in.GetID(), Nickname: in.GetNickname(), Likes: profile.Likes}
+	ret := &pb.ProfileReply{Id: in.GetId(), Nickname: in.GetNickname(), Likes: profile.Likes}
 	tracer.Trace(time.Now().UTC(), ret)
 	return ret, nil
 }
@@ -108,7 +108,7 @@ func (s *server) UpdateProfile(ctx context.Context, in *pb.ProfileRequest) (*pb.
 	tracer.Trace(time.Now().UTC(), in)
 	if len(in.GetLikes()) > 1000 {
 		tracer.Trace(time.Now().UTC(), in, "Too Many Likes")
-		ret := &pb.ProfileReply{ID: in.GetID(), Nickname: in.GetNickname(), Likes: in.GetLikes()}
+		ret := &pb.ProfileReply{Id: in.GetId(), Nickname: in.GetNickname(), Likes: in.GetLikes()}
 		return ret, nil
 	}
 	var datastoreClient *datastore.Client
@@ -118,16 +118,16 @@ func (s *server) UpdateProfile(ctx context.Context, in *pb.ProfileRequest) (*pb.
 		tracer.Trace(time.Now().UTC(), in, "Should Not Datastore New Client", err.Error())
 	}
 	var profile Profile
-	profile.ID = in.GetID()
+	profile.ID = in.GetId()
 	profile.Nickname = in.GetNickname()
 	profile.Likes = in.GetLikes()
-	key := datastore.IDKey("Profile", in.GetID(), nil)
+	key := datastore.IDKey("Profile", in.GetId(), nil)
 	_, err = datastoreClient.Put(ctx, key, &profile)
 	if err != nil {
 		log.Printf("Should Not Profile get: " + err.Error())
 		tracer.Trace(time.Now().UTC(), in, "Should Not Profile get", err.Error())
 	}
-	ret := &pb.ProfileReply{ID: in.GetID(), Nickname: in.GetNickname(), Likes: profile.Likes}
+	ret := &pb.ProfileReply{Id: in.GetId(), Nickname: in.GetNickname(), Likes: profile.Likes}
 	tracer.Trace(time.Now().UTC(), ret)
 	return ret, nil
 }
@@ -140,14 +140,14 @@ func (s *server) GetPoint(ctx context.Context, in *pb.PointRequest) (*pb.PointRe
 		log.Printf("Should Not Datastore New Client" + err.Error())
 		tracer.Trace(time.Now().UTC(), in, "Should Not Datastore New Client", err.Error())
 	}
-	key := datastore.IDKey("Rating", in.GetID(), nil)
+	key := datastore.IDKey("Rating", in.GetId(), nil)
 	var rating Rating
 	if err := datastoreClient.Get(ctx, key, &rating); err != nil {
 		log.Printf("Should Not Rating get: " + err.Error())
 		tracer.Trace(time.Now().UTC(), in, "Should Not Rating get", err.Error())
 	}
 	in.Point = rating.Point
-	ret := &pb.PointReply{ID: in.GetID(), Point: in.GetPoint(), Count: rating.Count}
+	ret := &pb.PointReply{Id: in.GetId(), Point: in.GetPoint(), Count: rating.Count}
 	tracer.Trace(time.Now().UTC(), ret)
 	return ret, nil
 }
@@ -161,29 +161,29 @@ func (s *server) IncrPoint(ctx context.Context, in *pb.PointRequest) (*pb.PointR
 		tracer.Trace(time.Now().UTC(), in, "Should Not Datastore New Client", err.Error())
 	}
 	var rating Rating
-	key := datastore.IDKey("Rating", in.GetID(), nil)
+	key := datastore.IDKey("Rating", in.GetId(), nil)
 	_ = datastoreClient.Get(ctx, key, &rating)
 	rating.Count += 1
 	rating.Point += in.GetPoint()
-	rating.ID = in.GetID()
+	rating.ID = in.GetId()
 	_, err = datastoreClient.Put(ctx, key, &rating)
 	if err != nil {
 		log.Printf("Should Not Incr Rating Point: " + err.Error())
 		tracer.Trace(time.Now().UTC(), in, "Should Not Rating get", err.Error())
 	}
 	in.Point = rating.Point
-	ret := &pb.PointReply{ID: in.GetID(), Point: in.GetPoint(), Count: rating.Count}
+	ret := &pb.PointReply{Id: in.GetId(), Point: in.GetPoint(), Count: rating.Count}
 	tracer.Trace(time.Now().UTC(), ret)
 	return ret, nil
 }
 
-func (s *server) GetPlace(ctx context.Context, in *pb.PlaceRequest) (*pb.PlaceReply, error) {
+func (s *server) GetPlaceByInput(ctx context.Context, in *pb.PlaceRequest) (*pb.PlaceReply, error) {
 	tracer.Trace(time.Now().UTC(), in)
 	// todo cache
 	PlaceID := FindPlaceFromText.Find(*apiKey, in.GetInput(), *inputType)
 	// todo cache
 	if PlaceID == "0" {
-		return &pb.PlaceReply{PlaceID: "0"}, nil
+		return &pb.PlaceReply{PlaceId: "0"}, nil
 	}
 	var placeReply pb.PlaceReply
 	marsharled, _ := json.Marshal(PlaceDetails.Find(*apiKey, PlaceID, in.GetLanguage()))
@@ -192,10 +192,27 @@ func (s *server) GetPlace(ctx context.Context, in *pb.PlaceRequest) (*pb.PlaceRe
 	return &placeReply, nil
 }
 
+func (s *server) GetPlaceByID(ctx context.Context, in *pb.PlaceRequest) (*pb.PlaceReply, error) {
+	tracer.Trace(time.Now().UTC(), in)
+	var placeReply pb.PlaceReply
+	place := PlaceDetails.Find(*apiKey, in.GetPlaceId(), in.GetLanguage())
+	marsharled, _ := json.Marshal(place)
+	tracer.Trace(time.Now().UTC(), string(marsharled))
+	_ = json.Unmarshal(marsharled, &placeReply)
+	tracer.Trace(time.Now().UTC(), &placeReply)
+	return &placeReply, nil
+}
+
 type PlaceProifle struct {
-	PlaceID      string
-	Populartimes []int64
-	Likes        int64
+	PlaceID      string         `json:"place_id,omitempty"`
+	Populartimes []Populartimes `json:"popular_times,omitempty"`
+	Likes        int64          `json:"likes,omitempty"`
+}
+
+type Populartimes struct {
+	Weekday int64 `json:"weekday,omitempty"`
+	Time    int64 `json:"time,omitempty"`
+	Count   int64 `json:"count,omitempty"`
 }
 
 // UpdatePlace updatePlace additional info
@@ -208,22 +225,37 @@ func (s *server) UpdatePlaceProfile(ctx context.Context, in *pb.PlaceRequest) (*
 		tracer.Trace(time.Now().UTC(), in, "Should Not Datastore New Client", err.Error())
 	}
 	var placeProfile PlaceProifle
-	_ = datastoreClient.Get(ctx, datastore.NameKey("Place", in.GetPlaceID(), nil), &placeProfile)
+	_ = datastoreClient.Get(ctx, datastore.NameKey("Place", in.GetPlaceId(), nil), &placeProfile)
 	if placeProfile.PlaceID == "" {
-		placeProfile.PlaceID = in.GetPlaceID()
+		placeProfile.PlaceID = in.GetPlaceId()
 	}
 	if in.IsLike {
 		placeProfile.Likes += 1
 	}
-	// todo 시간대별 방문수 누적
-	_, err = datastoreClient.Put(ctx, datastore.NameKey("Place", in.GetPlaceID(), nil), &placeProfile)
+	if in.IsVisit {
+		isHit := false
+		WeekdayNow := int64(time.Now().UTC().Weekday())
+		TimeNow := int64(time.Now().UTC().Hour())
+		for k, v := range placeProfile.Populartimes {
+			if v.Weekday == WeekdayNow && v.Time == TimeNow {
+				placeProfile.Populartimes[k].Count++
+				isHit = true
+			}
+		}
+		if !isHit {
+			placeProfile.Populartimes = append(placeProfile.Populartimes, Populartimes{Weekday: WeekdayNow, Time: TimeNow, Count: 1})
+		}
+	}
+	_, err = datastoreClient.Put(ctx, datastore.NameKey("Place", in.GetPlaceId(), nil), &placeProfile)
 	if err != nil {
 		log.Printf("Should Not Update Place: " + err.Error())
 		tracer.Trace(time.Now().UTC(), in, "Should Not Profile get", err.Error())
 	}
-	ret := &pb.PlaceProfileReply{PlaceID: placeProfile.PlaceID, Likes: placeProfile.Likes}
-	tracer.Trace(time.Now().UTC(), ret)
-	return ret, nil
+	var PlaceProfileReply pb.PlaceProfileReply
+	marsharled, _ := json.Marshal(placeProfile)
+	_ = json.Unmarshal(marsharled, &PlaceProfileReply)
+	tracer.Trace(time.Now().UTC(), &PlaceProfileReply)
+	return &PlaceProfileReply, nil
 }
 
 func (s *server) GetNearbySearch(ctx context.Context, in *pb.PlaceRequest) (*pb.PlaceReplyList, error) {
